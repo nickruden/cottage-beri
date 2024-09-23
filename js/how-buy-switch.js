5862d910e2f9578e0e7448cf717e8de791505dac
nickruden <1nikitos.ru2018@gmail.com>
2024-09-23 12:58:49 +0300

    Исправление формы Как купить, правки отступов

--- js/how-buy-switch.js
+++ js/how-buy-switch.js
@@ -1,17 +1,24 @@
 // СКРИПТ ДЛЯ ПЕРЕКЛЮЧЕНИЯ КНОПКОК В БЛОКАХ: КАК ЗАКАЗАТЬ СТРОИТЕЛЬСТВО 
-document.querySelectorAll('.how-buy__button').forEach(button => {
-    button.addEventListener('click', function () {
-        const tab = this.getAttribute('data-tab');
+    let hiddenInput = document.getElementById('selected-tab-input');
+    console.log('Updated hidden input value:', hiddenInput.value)
 
-        document.querySelectorAll('.how-buy__payment-type-descriprion').forEach(desc => {
-            desc.style.display = desc.id === tab ? 'block' : 'none';
-        }); // показываем блок, который по id совпадает с data-tab
+    document.querySelectorAll('.how-buy__button').forEach(button => {
+        button.addEventListener('click', function (event) {
+            const tab = event.target.getAttribute('data-tab');
 
-        document.querySelectorAll('.how-buy__button').forEach(btn => {
-            btn.classList.remove('how-buy__button_active');
+            hiddenInput.value = tab;
+
+            // Обновляем отображение описания выбранного типа оплаты
+            document.querySelectorAll('.how-buy__payment-type-descriprion').forEach(desc => {
+                desc.style.display = desc.id === tab ? 'block' : 'none';
+            });
+
+            // Обновляем активный класс на кнопках
+            document.querySelectorAll('.how-buy__button').forEach(btn => {
+                btn.classList.remove('how-buy__button_active');
+            });
+            
+            event.target.classList.add('how-buy__button_active');
+            console.log('Updated hidden input value:', hiddenInput.value);
         });
-
-        this.classList.add('how-buy__button_active');
-        document.getElementById('tab').value = tab;
     });
-});
